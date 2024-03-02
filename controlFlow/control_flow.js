bee50f87aac4759adc26677dd09c2086009ab007
Priyanshu <Rose@IdeaPad.localdomain>
2024-03-02 23:06:39 +0530

    3rd commit

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -39,3 +39,27 @@ console.log("User Category:", userCategory);
 let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 console.log("Authentication Status:", authenticationStatus);
+
+//////////////////////////////////////////
+let randm = Math.floor(Math.random() * 4);
+let arr = ["Employee", "Enrolled Member", "Subscriber", "Non-Subscriber"];
+let person = arr[randm];
+console.log(`You are '${person}'`);
+
+switch(person){
+    case "Employee":
+        console.log("authorized to have access to \"Dietary Services\"");
+        break;
+    case "Enrolled Member":
+        console.log("authorized to have access to \"Dietary Services\" and one-on-one interaction with a dietician");
+        break;
+    case "Subscriber":
+        console.log("authorized to have partial access to facilitate \"Dietary Services\" only");
+        break;
+    case "Non-Subscriber":
+        console.log("need to enroll or at least subscribe first to avail this facility");
+        break;
+    default:
+        console.log("Invalid ");
+        break;
+}
